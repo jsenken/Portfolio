b88f0677758fd64b67269bffc18d3541a5f48117
Jordan Senken <jsenken@yahoo.com>
2018-11-07 23:19:56 -0500

    Updates

---
+++ precache-manifest.b8ea1b284d3ca41cb80245508d3ebeb5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b59b3ac84b3974306385390ec49d03",
+    "url": "/Portfolio/static/media/suit.a0b59b3a.png"
+  },
+  {
+    "revision": "5287b79923513eecff2de6b8137c4f77",
+    "url": "/Portfolio/static/media/store.5287b799.png"
+  },
+  {
+    "revision": "3621ae4ad267535271137d5e2936b0a3",
+    "url": "/Portfolio/static/media/pool.3621ae4a.png"
+  },
+  {
+    "revision": "7f6f1b4e2fcb51b0f86234706c0dbef7",
+    "url": "/Portfolio/static/media/news.7f6f1b4e.jpg"
+  },
+  {
+    "revision": "df15149fb9001c705789a3c2f908c144",
+    "url": "/Portfolio/static/media/blood.df15149f.png"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "ffa3fa7e3b9b5277f62b",
+    "url": "/Portfolio/static/js/main.ffa3fa7e.chunk.js"
+  },
+  {
+    "revision": "65407b2d05b9c494f006",
+    "url": "/Portfolio/static/js/1.65407b2d.chunk.js"
+  },
+  {
+    "revision": "ffa3fa7e3b9b5277f62b",
+    "url": "/Portfolio/static/css/main.09e989a1.chunk.css"
+  },
+  {
+    "revision": "69059b4feae5b1eec9a0e3180beefcca",
+    "url": "/Portfolio/index.html"
+  }
+];
